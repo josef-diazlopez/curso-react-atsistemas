96d77ff36accb20e92ad0855dc3a46f0429f9d50
Marco Antonio Romero Martín <maromero@digitalwolves.com.es>
2020-10-21 20:06:14 +0200

    add hooks

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from "react";
 import "./App.css";
-import { Card } from "./components/Card/Card";
-import { Body } from "./components/Card/Body/Body";
+import { Users } from "./pages/Users";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Card name="Jose">
-          <Body texts={["Párrafo 1", "Párrafo 2"]}></Body>
-        </Card>
-      </header>
+      <Users></Users>
     </div>
   );
 }
