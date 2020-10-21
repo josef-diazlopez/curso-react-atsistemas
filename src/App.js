fa4bf9c9064647f519a70f4665de552fea63c176
Sara Alamillo Arroyo <salamillo@atsistemas.com>
2020-10-21 17:59:55 +0200

    Created card component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./App.css";
 import { Card } from "./components/Card/Card";
+import { Body } from "./components/Card/Body/Body";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Card name='Jose'/>
+        <Card name="Jose">
+          <Body texts={["Párrafo 1", "Párrafo 2"]}></Body>
+        </Card>
       </header>
     </div>
   );
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,26 +1,22 @@
-import React from "react"
+import React from "react";
 import "./Card.css";
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
 
-export const Card = ({name}) =>{
-    return(
-        <div className="card">
-            <div className="float-btn">
-                boton flotante
-            </div>
-            <div className="card-header">
-                <h1>{name}</h1>
-            </div>
-            <div className="card-body">
-                <p>Cuerpo</p>
-            </div>
-            <div className="card-footer">
-                <p>redes sociales</p>
-            </div>
-        </div>
-    )
-}
+export const Card = ({ name, children }) => {
+  return (
+    <div className="card">
+      <div className="float-btn">boton flotante</div>
+      <div className="card-header">
+        <h1>{name}</h1>
+      </div>
+      <div className="card-body">{children}</div>
+      <div className="card-footer">
+        <p>redes sociales</p>
+      </div>
+    </div>
+  );
+};
 
 Card.propTypes = {
-name: PropTypes.string.isRequired
-}
+  name: PropTypes.string.isRequired,
+};
