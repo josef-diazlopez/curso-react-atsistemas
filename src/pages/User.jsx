b0165a01ca78d8b3657aab451691fe2be8edac36
Sara Alamillo Arroyo <salamillo@atsistemas.com>
2020-10-22 18:03:39 +0200

    Created useGetUsers hook and User class

---
+++ src/pages/User.jsx
@@ -0,0 +1,80 @@
+import React, { Component, useEffect } from "react";
+import PropTypes from "prop-types";
+import axios from "axios";
+
+const API_USERS = "https://reqres.in/api/users/";
+
+export class User extends Component {
+  constructor(props) {
+    super(props);
+    // this.state = {
+    //   currentId: 1,
+    // };
+  }
+
+  render() {
+    const data = this.state.data?.data;
+    const ad = this.state.data?.ad;
+
+    return (
+      <>
+        <div>
+          <img src={data?.avatar}></img>
+          <p>{data?.id}</p>
+          <p>{data?.email}</p>
+          <p>{`${data?.first_name} ${data?.last_name}`}</p>
+        </div>
+
+        <div>
+          <p>
+            <a href={ad?.url}>{ad?.company}</a>
+          </p>
+          <p>{ad?.text}</p>
+        </div>
+
+        <div>
+          <button
+            onClick={() => {
+              this.setState({ data: { id: data.id + 1 } });
+            }}
+
+            // onClick={() => {
+            //   const currentId = this.state.currentId + 1;
+            //   this.setState({ currentId });
+            //   this.loadUser(currentId);
+            // }}
+          >
+            Siguiente usuario
+          </button>
+        </div>
+      </>
+    );
+  }
+
+  componentDidMount() {
+    // this.loadUser(this.state.currentId);
+    this.loadUser(1);
+  }
+
+  loadUser(id) {
+    axios
+      .get(API_USERS + id)
+      .then((response) => {
+        this.setState({
+          data: response?.data,
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  componentDidUpdate(previousProps, previousState) {
+    if (
+      this.state.data?.id &&
+      previousState?.data?.id !== this.state.data?.id
+    ) {
+      this.loadUser(this.state.data.id);
+    }
+  }
+}
--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,43 +1,23 @@
-import React, { useEffect, useState } from 'react';
-import PropTypes from 'prop-types';
-import { Card } from './../components/Card/Card';
-import { Body } from './../components/Card/Body/Body';
-import axios from 'axios';
+import React from "react";
+import PropTypes from "prop-types";
+import { Card } from "./../components/Card/Card";
+import { Body } from "./../components/Card/Body/Body";
+import { useGetUsers } from "../hooks/users/useGetUsers";
 
-const API_USERS = 'https://reqres.in/api/users';
+export const Users = ({}) => {
+  const data = useGetUsers();
 
-export const Users = ({  }) => {
-    const [data, setData] = useState([]);
-    useEffect(() => {
-        axios.get(API_USERS)
-            .then((response) => {
-                setData(response.data.data);
-            })
-            .catch((error) => {
-                console.error(error);
-            })
-    },[])
-    return (
-        <header className="App-header">
-        {
-            data.map((user) => (
-                <Card name={`${user.first_name} ${user.last_name}`} key={user.id}>
-                    <Body texts={[user.email]}></Body>
-                </Card>
-            ))
-        }
-
-      </header>
-    );
+  return (
+    <header className="App-header">
+      {data.map((user) => (
+        <Card name={`${user.first_name} ${user.last_name}`} key={user.id}>
+          <Body texts={[user.email]}></Body>
+        </Card>
+      ))}
+    </header>
+  );
 };
 
-Users.propTypes = {
+Users.propTypes = {};
 
-}
-
-Users.defaultProps = {
-
-}
-
-
-
+Users.defaultProps = {};
