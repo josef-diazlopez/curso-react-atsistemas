96f9dd734905faa36f8809d6abefbcf294aab522
Jesus Macias <maciasfelixj@gmail.com>
2020-10-22 16:25:08 +0200

    feat: create useGetUsers hooks to get data from backend

---
+++ src/hooks/users/useGetUsers.js
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const API_USERS = "https://reqres.in/api/users";
+
+export const useGetUsers = () => {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    axios
+      .get(API_USERS)
+      .then((response) => {
+        setUsers(response.data.data);
+      })
+      .catch((error) => {
+        //TODO:
+        console.error(error);
+      });
+  }, []);
+  return users;
+};
