bed8071541374cfde141201788a78e231886e51d
Ruben Gonzalez <rgonzalez.membrilla@atsistemas.com>
2020-10-22 16:31:14 +0200

    Fixed vars names

--- src/components/hooks/useCallUsers.jsx
+++ src/components/hooks/useCallUsers.jsx
@@ -4,20 +4,20 @@ import axios from 'axios'
 const API_USERS = 'https://reqres.in/api/users'
 
 const useCallUsers = () => {
-    const [users, setUsers] = useState([])
+    const [data, setData] = useState([])
 
     useEffect(() => {
         axios
             .get(API_USERS)
             .then((response) => {
-                setUsers(response.data.data)
+                setData(response.data.data)
             })
             .catch((error) => {
                 console.error(error)
             })
     }, [])
 
-    return users
+    return data
 }
 
 export default useCallUsers
