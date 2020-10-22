b73691a0ce2ce5fe7413943be3480744a9f92cbf
Jesus Macias <maciasfelixj@gmail.com>
2020-10-22 19:29:33 +0200

    feat: Using useHistory to navigate

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,17 +3,29 @@ import "./Card.css";
 import PropTypes from "prop-types";
 import { FooterSocial } from "./FooterSocial/FooterSocial";
 import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 export const Card = ({ name, children, id }) => {
+  const history = useHistory();
+  const navigateRoute = () => {
+    if (window.confirm("Do you really want to leave?")) {
+      history.push(`/user/${id}`);
+    }
+  };
   return (
     <div className="card">
       <div className="float-btn">boton flotante</div>
       <div className="card-header">
-        <Link style={{textDecoration: "none", color: "black"}} to={`/user/${id}`}>
+        <Link
+          style={{ textDecoration: "none", color: "black" }}
+          to={`/user/${id}`}
+        >
           <h1>{name}</h1>
         </Link>
       </div>
-      <div className="card-body">{children}</div>
+      <div className="card-body" onClick={navigateRoute}>
+        {children}
+      </div>
       <div className="card-footer">
         <FooterSocial></FooterSocial>
       </div>
@@ -23,4 +35,5 @@ export const Card = ({ name, children, id }) => {
 
 Card.propTypes = {
   name: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
 };
