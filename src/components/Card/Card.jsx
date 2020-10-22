338ffc9b3c31a09b41dac99395e5c95df5544419
Adrián <ajrodriguez.garcia@atsistemas.com>
2020-10-22 19:26:19 +0200

    add useHistory

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,10 +3,11 @@ import "./Card.css";
 import PropTypes from "prop-types";
 import { FooterSocial } from "./FooterSocial/FooterSocial";
 import {
-  Link,
+  Link, useHistory
 } from "react-router-dom";
 
 export const Card = ({ name, children, idUser}) => {
+  const history = useHistory()
   return (
     <div className="card">
       <div className="float-btn">boton flotante</div>
@@ -15,7 +16,11 @@ export const Card = ({ name, children, idUser}) => {
         <h1>{name}</h1>
         </Link>
       </div>
-      <div className="card-body">{children}</div>
+      <div className="card-body" onClick={() => {
+          if(window.confirm('¿Quiere navegar al detalle del usuario?')){
+            history.push(`user/${idUser}`)}
+        }}
+        >{children}</div>
       <div className="card-footer">
         <FooterSocial></FooterSocial>
       </div>
